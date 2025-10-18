4ca8073adb3e48abf9d7334753439c544420533a
Daniel <daniel2000r@gmail.com>
2025-10-18 16:45:42 +0200

    WIP v2

--- Lab02/Zad01.py
+++ Lab02/Zad01.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
+def classify_iris(sl, sw, pl, pw):
+    if sl > 4:
+        return("Setosa")
+    elif pl <= 5:
+        return("Virginica")
+    else:
+        return("Versicolor")
+
+
 df = pd.read_csv("iris_big.csv")
 
 (train_set, test_set) = train_test_split (df.values, train_size=0.7,
@@ -9,5 +19,15 @@ train_inputs = train_set[:, 0:4]
 train_classes = train_set[:, 4]
 test_inputs = test_set[:, 0:4]
 test_classes = test_set[:, 4]
+good_predictions = 0
+len = test_set.shape[0]
 
-print(test_set)
+for i in range(len):
+    if classify_iris(test_inputs[i][0], test_inputs[i][1],
+                     test_inputs[i][2], test_inputs[i][3]) == test_classes[i]:
+        good_predictions += 1
+
+print(good_predictions)
+print(good_predictions / len * 100, "%")
+
+
